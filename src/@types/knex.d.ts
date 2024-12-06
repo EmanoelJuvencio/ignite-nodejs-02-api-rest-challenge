e7caa502e30e5a118db3d7e82a9d37a265a755ca
Emanoel <emanoeljuvencio@gmail.com>
2024-12-06 15:46:28 -0300

    chore: implementando api

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -2,6 +2,24 @@
 
 declare module 'knex/types/tables' {
   export interface Tables {
-    // TODO
+    users: {
+      id: string
+      session_id: string
+      name: string
+      email: string
+      created_at: string
+      updated_at: string
+    }
+
+    meals: {
+      id: string
+      user_id: string
+      name: string
+      description: string
+      is_on_diet: boolean
+      date: number
+      created_at: string
+      updated_at: string
+    }
   }
 }
